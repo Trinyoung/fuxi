4af94e8236fc1ad575e559f690b867962285b7fe
trinyoung <trinyoung@163.com>
2020-11-24 14:43:08 +0800

    fix(app): 修改启动文件

--- app.ts
+++ app.ts
@@ -1,8 +1,8 @@
 /*
  * @Author: Trinyoung.Lu
  * @Date: 2020-09-23 15:22:48
- * @LastEditors: Trinyoung.Lu
- * @LastEditTime: 2020-10-05 18:10:35
+ * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2020-11-24 14:42:35
  * @PageTitle: XXX页面
  * @Description: XXX
  * @FilePath: \fuxi\app.ts
@@ -14,9 +14,11 @@ import route from './router';
 import { Logger } from './logger/config';
 
 const app = new Koa();
-const router =  new Router({
-	prefix: '/api'
-});
+const router =  new Router(
+	{
+		prefix: '/api'
+	}
+);
 route(router);
 app
 	.use(bodyParser())
