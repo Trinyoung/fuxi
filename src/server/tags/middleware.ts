9b12128d5d9f90cf56728c1fdb26723b1f41e986
trinyoung <trinyoung@163.com>
2021-03-16 09:25:37 +0800

    fix(tsconfig): tsconfig 配置修改

--- src/server/tags/middleware.ts
+++ src/server/tags/middleware.ts
@@ -2,7 +2,7 @@
  * @Author: Trinyoung.Lu
  * @Date: 2020-10-19 15:57:57
  * @LastEditors: Please set LastEditors
- * @LastEditTime: 2021-01-20 14:05:22
+ * @LastEditTime: 2021-03-16 09:23:50
  * @PageTitle: XXX页面
  * @Description: XXX
  * @FilePath: \fuxi\server\tags\middleware.ts
@@ -11,10 +11,10 @@ import { Next, ParameterizedContext } from 'koa';
 import { Logger } from '../../logger/config';
 
 import cacheHelper from '../../cacheHelper';
-export async function getTagsFromCache (ctx: ParameterizedContext, next: Next) {
+export async function getTagsFromCache(ctx: ParameterizedContext, next: Next) {
     let tagkey = ctx.cookies.get('tagKey');
     if (tagkey) {
-        const result = await new Promise((resolve, reject) => {
+        const result:any = await new Promise((resolve, reject) => {
             cacheHelper.get('tags', tagkey, (err, result) => {
                 if (err) {
                     Logger.info('获取token失败', err);
@@ -37,7 +37,8 @@ export async function getTagsFromCache (ctx: ParameterizedContext, next: Next) {
     const expireTime = now + 1000 * 60 * 60 * 48;
     ctx.cookies.set('tagKey', 'luqingyang', { expires: new Date(expireTime), httpOnly: true });
     tagkey = ctx.cookies.get('tagKey');
-    cacheHelper.put('tags', tagkey, ctx.body.list, res => {
-        // console.log(res, '---------------->')
+    const { list } = ctx.body as any;
+    cacheHelper.put('tags', tagkey, list, (res) => {
+        console.log(res, '---------------->')
     });
 };
