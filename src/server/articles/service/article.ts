6715a15b7c582aeb9b2ba2e5d2f7621f6f469a1a
trinyoung <trinyoung@163.com>
2023-10-25 01:10:29 +0800

    fix

--- src/server/articles/service/article.ts
+++ src/server/articles/service/article.ts
@@ -25,6 +25,7 @@ export class ArticleService extends BaseService<ArticleInterface> {
             favoriteModel.find({ articleId: { $in: ids } }).then((res:any) => { return Promise.resolve(this.objKeyByArticle(res)); }),
             commentModel.find({ articleId: { $in: ids } }).then((res:any) => { return Promise.resolve(this.objKeyByArticle(res)); })
         ]);
+        console.log(marked.parse(result.docs[0].content), 'getListByPageForAriticle ======content');
         const resArr = result.docs.map(item => {
             const readsObj = Object.assign({ total: 0, weekNums: 0, monthNums: 0 }, readsKeyByArticle[JSON.stringify(item._id)]);
             const favoriteObj = Object.assign({ total: 0, weekNums: 0, monthNums: 0 }, favoriteKeyByArticle[JSON.stringify(item._id)]);
